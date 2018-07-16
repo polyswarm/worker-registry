aaa0f308b46dada1752c0031c340310be72eadb9
Robert Lathrop <rl@polyswarm.io>
2018-07-16 16:48:17 -0400

    Implement adding and signing microengines.

--- cli/index.js
+++ cli/index.js
@@ -2,6 +2,8 @@ const figlet = require('figlet');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const _ = require('underscore');
+const keythereum = require('keythereum');
+const etherutils = require('ethereumjs-util');
 
 const selectAction = async () => {
   const action = [{
@@ -56,7 +58,91 @@ const enterDeveloper = async () => {
 };
 
 const addMicroengine = async () => {
+  const added = {};
 
+  const questions = [
+    {
+      type: "input",
+      name: "address",
+      message: "What is the address for the micro engine wallet?",
+    },
+    {
+      type: "input",
+      name: "tags",
+      message: "Enter a comma separated list of tags for this micro engine",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter a description of the micro engine (optional)",
+    },
+  ];
+
+  const engine = await inquirer.prompt(questions);
+
+  console.log(engine);
+
+  let signed = null;
+
+  do {
+    try {
+      signed = await sign(engine, engine.address);
+    } catch(error) {
+      console.log(`${chalk.red("X")} Missing keyfile or bad password.`);
+    }
+    if (signed == null) {
+      const tryAgain = [{
+        type: "confirm",
+        message: "Failed to unlock the keyfile. Try again? (No will remove this microengine)",
+        default: false,
+        name: "again",
+      }]
+      const choice = await inquirer.prompt(tryAgain);
+      if (!choice.confirm) {
+        return null;
+      }
+    }
+  } while (signed == null);
+
+  added.microengine = engine;
+  added.signed = signed;
+  return added;
+};
+
+const sign = async (object, address) => {
+  const signing = [
+    {
+      type: "input",
+      name: "keydir",
+      default: `/home/${require('os').userInfo().username}/.ethereum/`,
+      message: `Enter the path to the directory containing the keystore ${address}`
+    },
+    {
+      type: "password",
+      name: "password",
+      message: `Enter password for ${address}`
+    },
+  ];
+
+  const toSign = JSON.stringify(object);
+
+  const keyInfo = await inquirer.prompt(signing);
+  const key = keythereum.importFromFile(address, keyInfo.keydir);
+  const buff_key = keythereum.recover(keyInfo.password, key);
+
+  let msg =
+  '0x' +
+  etherutils.keccak(etherutils.toBuffer(toSign)).toString('hex');
+  msg =
+  '0x' +
+  etherutils
+    .hashPersonalMessage(etherutils.toBuffer(msg))
+    .toString('hex');
+  const sig = etherutils.ecsign(etherutils.toBuffer(msg), buff_key);
+  let r = '0x' + sig.r.toString('hex');
+  let s = '0x' + sig.s.toString('hex');
+  let v = sig.v;
+  return {r: r, s: s, v: v};
 };
 
 const validate = async () => {
@@ -88,12 +174,20 @@ const main = async () => {
         }
       });
     } else if (answer.action == "Add a microengine") {
-      
+      const added = await addMicroengine();
+      if (added) {
+        registryEntry.microengines.push(added.microengine);
+        registryEntry.signatures.push(added.signed);
+      } else {
+        console.log(`${chalk.red("X")} Cancelled microengine.`);
+      }
     } else if (answer.action == "Validate") {
 
     }
   }
-  
+
+  // Validate
+
   // Sign
   console.log(registryEntry);
 }
