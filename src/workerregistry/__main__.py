89adba1031647f204a23923fed947b87dbf71d2b
Rob Lathrop <rl@polyswarm.io>
2018-07-26 09:51:12 -0400

    Use options, instead of arguments.

--- src/workerregistry/__main__.py
+++ src/workerregistry/__main__.py
@@ -5,10 +5,10 @@ from gevent import pywsgi
 from workerregistry import create_app
 
 @click.command()
-@click.argument('--ipfs-uri', envvar='IPFS_URI', default=workerregistry.DEFAULT_IPFS_URI)
-@click.argument('--eth-uri', envvar='ETH_URI', default=workerregistry.DEFAULT_ETH_URI)
-@click.argument('--size-limit', envvar='SIZE_LIMIT', default=workerregistry.DEFAULT_SIZE_LIMIT)
-@click.argument('--registry-address', envvar='REGISTRY_ADDRESS', default=workerregistry.DEFAULT_REGISTRY_ADDRESS)
+@click.option('--ipfs-uri', envvar='IPFS_URI', default=workerregistry.DEFAULT_IPFS_URI)
+@click.option('--eth-uri', envvar='ETH_URI', default=workerregistry.DEFAULT_ETH_URI)
+@click.option('--size-limit', envvar='SIZE_LIMIT', default=workerregistry.DEFAULT_SIZE_LIMIT)
+@click.option('--registry-address', envvar='REGISTRY_ADDRESS', default=workerregistry.DEFAULT_REGISTRY_ADDRESS)
 def main(ipfs_uri, eth_uri, size_limit, registry_address):
     app = create_app(ipfs_uri, eth_uri, size_limit, registry_address)
     server = pywsgi.WSGIServer(('', 8000), app)
